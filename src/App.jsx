e3fe2192ae70a96f9f6dac24b7155b57aa2a3cc4
Anjelica <133686543+Anjie-MF@users.noreply.github.com>
2026-05-19 15:35:14 -0500

    Added second button to display active/disabled states

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 function App() {
   return (
     <button className="button">Join League</button>
+    <button className="button" disabled>Join League (disabled)</button>
   )
 }
 
